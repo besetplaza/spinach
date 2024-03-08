60a8e16a755c5d562a5e8aec8dc482a3a74cae9f
besetplaza <162570850+besetplaza@users.noreply.github.com>
2024-03-09 00:52:57 +0900

    Add files via upload

---
+++ js/common.js
@@ -0,0 +1,6 @@
+$(function() {
+    let sunday = _nextSunday(new Date());
+    if ($('next_sunday')) {
+        $('next_sunday').value(_format(sunday, 'YYYY-MM-DD'));
+    }
+});
---
+++ js/import.js
@@ -0,0 +1,5 @@
+
+import { format } from "https://unpkg.com/date-fns/format.mjs";
+let _format = new format();
+import { nextSunday } from "https://unpkg.com/date-fns/nextSunday.mjs";
+let _nextSunday = new nextSunday();
